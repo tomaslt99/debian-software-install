8df702d8b5c41434d9b2a5a8e3ccd54e8584bbeb
tomaslt99 <53668968+tomaslt99@users.noreply.github.com>
2025-07-25 08:33:16 +0300

    Add files via upload
    Script works.
    Needs:
    Display if script installed successfully.

--- debian-programs-auto-install.sh
+++ debian-programs-auto-install.sh
@@ -1,27 +1,66 @@
 #!/bin/bash
 
-set -e
-
 # Update package list
 sudo apt update
+sudo apt upgrade -y
 
-# Install essential tools
-sudo apt install -y curl gnupg apt-transport-https software-properties-common
+# Brave
+curl -fsS https://dl.brave.com/install.sh | sh
+
+# Chromium
+sudo apt install chromium -y
+
+# Firefox
+sudo apt-get install firefox-esr -y
+
+# vivaldi
+wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor | sudo tee /usr/share/keyrings/vivaldi-browser.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=amd64] https://repo.vivaldi.com/archive/deb/ stable main" | sudo tee /etc/apt/sources.list.d/vivaldi.list
+sudo apt update
+sudo apt install vivaldi-stable -y
+
+# librewolf
+sudo apt update && sudo apt install extrepo -y
+sudo extrepo enable librewolf
+sudo apt update && sudo apt install librewolf -y
+
+# vlc
+sudo apt install vlc -y
+
+# virt-manager
+sudo apt-get install virt-manager -y
+
+# telegram-desktop
+sudo apt-get install telegram-desktop -y
+
+
+# RustDesk
+sudo apt install -y wget xvfb libgtk-3-0 libnotify4 libglib2.0-0 libnss3 libxss1 libasound2
+wget https://github.com/rustdesk/rustdesk/releases/download/1.3.3/rustdesk-1.3.3-x86_64.deb
+sudo dpkg -i rustdesk-1.3.3-x86_64.deb
+sudo apt install -f  # Fix missing dependencies
+# at this time only this way can install Rustdesk so after installling RustDesk Upgrade package list
 
 # Install Codium
-curl -fsSL https://packages.vscodium.com/pub.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/vscodium-archive-keyring.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg] https://packages.vscodium.com/debs vscodium main" | sudo tee /etc/apt/sources.list.d/vscodium.list
+# MIGHT NEED FIX IT
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg     | gpg --dearmor     | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+echo 'deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg] https://download.vscodium.com/debs vscodium main'     | sudo tee /etc/apt/sources.list.d/vscodium.list
+sudo apt update && sudo apt install codium
+
+
+#---
+#Setting up Network Time Protocol (NTP) on Debian 12
+
+# Install ntpd
 sudo apt update
-sudo apt install -y codium
+sudo apt install ntp -y
 
-# Install Brave Browser
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install -y brave-browser
+# Configure ntpd
+sudo tee /etc/ntp.conf > /dev/null << 'EOF'
+server 0.debian.pool.ntp.org iburst
+server 1.debian.pool.ntp.org iburst
+EOF
 
-# Install Firefox ESR
-sudo apt install -y firefox-esr
-
-# Install LibreWolf
-echo "deb [arch=amd64] http://deb.librewolf
+# Restart and Check Sync
+sudo systemctl restart ntp
+#---
